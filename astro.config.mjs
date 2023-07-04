b928345141abf46a73294db269bdb8b18667b800
Finn Harman <finnharman@gmail.com>
2023-07-04 23:17:13 +0100

    started first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://finn-harman.github.io',
-    base: '/fi-the-website',
+  site: 'https://finn-harman.github.io',
+  base: '/fi-the-website',
+  integrations: [mdx()]
 });
